ebcc3324c62e7c27ea2b6350c8ee6d868ea434f7
Tommy Chheng <tommy@tumblr.com>
2015-10-27 14:10:06 -0700

    basic layout

--- app/src/main/java/com/tommychheng/instagram/views/PostsAdapter.java
+++ app/src/main/java/com/tommychheng/instagram/views/PostsAdapter.java
@@ -3,6 +3,7 @@ package com.tommychheng.instagram.views;
 import android.content.Context;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.codepath.instagram.R;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.tommychheng.instagram.helpers.Utils;
 import com.tommychheng.instagram.models.InstagramPost;
 
 import java.util.List;
@@ -40,7 +42,20 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHol
     public void onBindViewHolder(PostsViewHolder holder, int position) {
         InstagramPost post = posts.get(position);
 
-        holder.tvCaption.setText(post.caption);
+        holder.tvUsername.setText(post.user.userName);
+        holder.tvDate.setText(DateUtils.getRelativeTimeSpanString(post.createdTime * 1000, System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS));
+
+        if (post.likesCount > 0) {
+            String likes = String.valueOf(post.likesCount) + " likes";
+            holder.tvLikes.setText(likes);
+        }
+
+        if (post.caption != null) {
+            holder.tvCaption.setText(post.user.userName + " - " + post.caption);
+        }
+
+        Uri profileImageUri = Uri.parse(post.user.profilePictureUrl);
+        holder.ivProfileImage.setImageURI(profileImageUri );
 
         Uri imageUri = Uri.parse(post.image.imageUrl);
         holder.ivImage.setImageURI(imageUri);
@@ -52,12 +67,20 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHol
     }
 
     public static class PostsViewHolder extends RecyclerView.ViewHolder {
+        public SimpleDraweeView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvDate;
+        public TextView tvLikes;
         public TextView tvCaption;
         public SimpleDraweeView ivImage;
 
         public PostsViewHolder(View layoutView) {
             super(layoutView);
+            tvUsername = (TextView) layoutView.findViewById(R.id.tvUsername);
+            tvDate = (TextView) layoutView.findViewById(R.id.tvDate);
+            tvLikes = (TextView) layoutView.findViewById(R.id.tvLikes);
             tvCaption = (TextView) layoutView.findViewById(R.id.tvCaption);
+            ivProfileImage = (SimpleDraweeView) layoutView.findViewById(R.id.ivProfileImage);
             ivImage = (SimpleDraweeView) layoutView.findViewById(R.id.ivImage);
         }
     }
