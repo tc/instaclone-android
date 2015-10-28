ab3afa96ef26f91fd7709518cbeca192e19fcc30
Tommy Chheng <tommy@tumblr.com>
2015-10-27 20:54:33 -0700

    margins

--- app/src/main/java/com/tommychheng/instagram/views/PostsAdapter.java
+++ app/src/main/java/com/tommychheng/instagram/views/PostsAdapter.java
@@ -3,7 +3,11 @@ package com.tommychheng.instagram.views;
 import android.content.Context;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.format.DateUtils;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +26,7 @@ import java.util.List;
  */
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHolder> {
     List<InstagramPost> posts;
+    Context context;
 
     public PostsAdapter(List<InstagramPost> posts) {
         this.posts = posts;
@@ -29,7 +34,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHol
 
     @Override
     public PostsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
+        context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
         View view = inflater.inflate(R.layout.activity_post, parent, false);
@@ -46,12 +51,14 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHol
         holder.tvDate.setText(DateUtils.getRelativeTimeSpanString(post.createdTime * 1000, System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS));
 
         if (post.likesCount > 0) {
-            String likes = String.valueOf(post.likesCount) + " likes";
+            String likes = Utils.formatNumberForDisplay(post.likesCount) + " likes";
             holder.tvLikes.setText(likes);
         }
 
         if (post.caption != null) {
-            holder.tvCaption.setText(post.user.userName + " - " + post.caption);
+            holder.tvCaption.setText(formatCaption(post.user.userName, post.caption), TextView.BufferType.EDITABLE);
+        } else {
+            holder.tvCaption.setVisibility(View.GONE);
         }
 
         Uri profileImageUri = Uri.parse(post.user.profilePictureUrl);
@@ -59,6 +66,19 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHol
 
         Uri imageUri = Uri.parse(post.image.imageUrl);
         holder.ivImage.setImageURI(imageUri);
+    }
+
+    public SpannableStringBuilder formatCaption(String userName, String caption) {
+        SpannableStringBuilder ssb = new SpannableStringBuilder(userName + " - " + caption);
+
+        ForegroundColorSpan colorSpan = new ForegroundColorSpan(context.getResources().getColor(R.color.blue_text));
+        ssb.setSpan(
+                colorSpan,
+                0,
+                userName.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return ssb;
     }
 
     @Override
